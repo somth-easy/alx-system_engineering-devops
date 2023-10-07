9e20fb18bb0f936dfa4a345efcc075eba0cdec93
somth-easy <somth_dope@yahoo.com>
2023-10-07 06:30:11 +0000

    regex

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,10 +1,4 @@
 #!/usr/bin/env ruby
 
-pattern = /\[from:([^]]+)?\] \[to:([^]]+)?\] \[flags:([^]]+)?\]/
-File.foreach(ARGV[0]) do |line|
-    matches = line.scan(pattern)
-      matches.each do |match|
-            puts match.join(' ') unless match.empty?
-              end
-end
+puts ARGV[0].scan(/\[from:([^]]+)?\] \[to:([^]]+)?\] \[flags:([^]]+)?\]/).join(', ')
 
